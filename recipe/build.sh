047a29b33905f05566398f3315d9402432a21041
Marcel Martin <marcel.martin@scilifelab.se>
2017-03-08 23:33:08 +0100

    Build libbz2.so.1.0
    Based on conda-forge/staged-recipes#1977

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,3 +4,8 @@
 export CFLAGS="${CFLAGS} -Wall -Winline -O2 -g -D_FILE_OFFSET_BITS=64 -fPIC"
 
 make install PREFIX=${PREFIX} CFLAGS="$CFLAGS"
+
+# build shared library
+make -f Makefile-libbz2_so CFLAGS="$CFLAGS"
+ln -s libbz2.so.${PKG_VERSION} libbz2.so
+cp -d libbz2.so* $PREFIX/lib/
