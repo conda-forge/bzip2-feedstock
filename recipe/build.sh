b38494f06669f39e537709b9c606535929074781
Marcel Martin <marcel.martin@scilifelab.se>
2017-03-10 11:05:55 +0100

    Build libbz2.dylib on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,13 @@ export CFLAGS="${CFLAGS} -Wall -Winline -O2 -g -D_FILE_OFFSET_BITS=64 -fPIC"
 make install PREFIX=${PREFIX} CFLAGS="$CFLAGS"
 
 # build shared library
-make -f Makefile-libbz2_so CFLAGS="$CFLAGS"
-ln -s libbz2.so.${PKG_VERSION} libbz2.so
-cp -d libbz2.so* $PREFIX/lib/
+if test `uname` = "Linux"
+then
+  make -f Makefile-libbz2_so CFLAGS="$CFLAGS"
+  ln -s libbz2.so.${PKG_VERSION} libbz2.so
+  cp -d libbz2.so* $PREFIX/lib/
+else  # macOS
+  $CC -shared -Wl,-install_name -Wl,libbz2.dylib -o libbz2.${PKG_VERSION}.dylib blocksort.o huffman.o crctable.o randtable.o compress.o decompress.o bzlib.o
+  ln -s libbz2.${PKG_VERSION}.dylib libbz2.dylib
+  cp -d libbz2.*dylib $PREFIX/lib/
+fi
