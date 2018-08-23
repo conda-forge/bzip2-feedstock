3cf4f6e5d3cbc90e0929a73201079ae390e5e108
Jonathan Helmus <jjhelmus@gmail.com>
2018-08-22 21:45:43 -0500

    restore pre-sync OS detection in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,11 +8,12 @@ USED_CC=${GCC:-${CC}}
 make install PREFIX=${PREFIX} CFLAGS="${CFLAGS}" CC="${USED_CC}"
 
 # build shared library
-if [[ ${HOST} =~ .*linux.* ]]; then
+if test `uname` = "Linux"
+then
   make -f Makefile-libbz2_so CFLAGS="${CFLAGS}" CC="${USED_CC}"
   ln -s libbz2.so.${PKG_VERSION} libbz2.so
   cp -d libbz2.so* ${PREFIX}/lib/
-elif [[ ${HOST} =~ .*darwin.* ]]; then
+else  # macOS
   ${USED_CC} -shared -Wl,-install_name -Wl,libbz2.dylib -o libbz2.${PKG_VERSION}.dylib blocksort.o huffman.o crctable.o randtable.o compress.o decompress.o bzlib.o
   cp libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/
   ln -s libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/libbz2.dylib
