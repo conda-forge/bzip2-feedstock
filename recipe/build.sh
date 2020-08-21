1bcfe820546941d1c5df8f7f525166f2d2ac4afd
Isuru Fernando <isuruf@gmail.com>
2020-08-21 05:35:22 +0000

    Build shared library on osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ if [[ ${target_platform} =~ .*linux.* ]]; then
   make -f Makefile-libbz2_so CFLAGS="${CFLAGS}" CC="${USED_CC}"
   ln -s libbz2.so.${PKG_VERSION} libbz2.so
   cp -d libbz2.so* ${PREFIX}/lib/
-elif [[ ${target_platform} == "osx-64" ]]; then
+elif [[ ${target_platform} == osx-* ]]; then
   ${USED_CC} -shared -Wl,-install_name -Wl,libbz2.dylib -o libbz2.${PKG_VERSION}.dylib blocksort.o huffman.o crctable.o randtable.o compress.o decompress.o bzlib.o
   cp libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/
   ln -s libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/libbz2.dylib
