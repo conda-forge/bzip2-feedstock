0ca23381a18cc9833879fd2216fb893fc3500bbb
Isuru Fernando <isuruf@gmail.com>
2020-11-28 14:20:31 -0800

    Use LDFLAGS when building shared library on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,7 +13,7 @@ if [[ ${target_platform} =~ .*linux.* ]]; then
   ln -s libbz2.so.${PKG_VERSION} libbz2.so
   cp -d libbz2.so* ${PREFIX}/lib/
 elif [[ ${target_platform} == osx-* ]]; then
-  ${USED_CC} -shared -Wl,-install_name -Wl,libbz2.dylib -o libbz2.${PKG_VERSION}.dylib blocksort.o huffman.o crctable.o randtable.o compress.o decompress.o bzlib.o
+  ${USED_CC} -shared -Wl,-install_name -Wl,libbz2.dylib -o libbz2.${PKG_VERSION}.dylib blocksort.o huffman.o crctable.o randtable.o compress.o decompress.o bzlib.o ${LDFLAGS}
   cp libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/
   ln -s libbz2.${PKG_VERSION}.dylib ${PREFIX}/lib/libbz2.dylib
 fi
